5a130d074f948b1e922ab8faec0ead30f910e49d
bySabi Files <bysabi.files@gmail.com>
2019-04-06 02:43:18 +0200

    update gh-pages

--- example/next.config.js
+++ example/next.config.js
@@ -1,9 +1,6 @@
 const webpack = require('webpack');
 
-const isProd = (process.env.NODE_ENV || 'production') === 'production';
-const isDevelop = process.env.NODE_ENV === 'development';
-
-const assetPrefix = !isDevelop ? '/garfio' : '';
+const PREFIX = '/garfio';
 
 module.exports = {
   exportPathMap: () => ({
@@ -12,14 +9,5 @@ module.exports = {
     '/counterNested': { page: '/counterNested' },
     '/counters10x40': { page: '/counters10x40' },
   }),
-  assetPrefix: assetPrefix,
-  webpack: config => {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
-      })
-    );
-
-    return config;
-  },
+  assetPrefix: PREFIX,
 };
--- example/pages/index.js
+++ example/pages/index.js
@@ -1,21 +1,23 @@
-import Link from '../components/link';
+import Link from 'next/link';
+
+const PREFIX = '/garfio';
 
 export default () => (
   <div style={{ textAlign: 'center' }}>
     <h1>Garfio usage examples</h1>
     <ul style={{ listStyle: 'none' }}>
       <li>
-        <Link href="/counterNested">
+        <Link href={`${PREFIX}/counterNested`}>
           <a>Nested Counters with "Container"</a>
         </Link>
       </li>
       <li>
-        <Link href="/counters10x40">
+        <Link href={`${PREFIX}/counters10x40`}>
           <a>Counters 10x40 with "Store"</a>
         </Link>
       </li>
       <li>
-        <Link href="/counter">
+        <Link href={`${PREFIX}/counter`}>
           <a>Counter with "Simple"</a>
         </Link>
       </li>
